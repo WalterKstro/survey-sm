474dbce9391f28209e164ab32b395c10a0dd02bb
Walter Castro <walterkstro@gmail.com>
2023-06-05 10:06:43 -0600

    Listar registros cambiado a tablas

--- resources/views/dashboard/country/index.blade.php
+++ resources/views/dashboard/country/index.blade.php
@@ -1,16 +1,31 @@
 <x-layouts.layout-dashboard>
     <h1 class="text-4xl">Paises registrados</h1>
     <a href="{{route('country.create')}}" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registrar nuevo pais</a>
-    <ul>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">País</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Eliminar</th>
+        </tr>
+        </thead>
+        <tbody>
         @foreach( $countries as $country )
-            <li>
-                {{ $country->name  }}
-                <a href="{{ route('country.edit',$country)  }}" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Editar</a>
-                <form action="{{route('country.destroy',$country)}}" method="POST">
-                    @csrf @method('DELETE')
-                    <button type="submit" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Borrar</button>
-                </form>
-            </li>
+            <tr>
+                <th scope="row">{{$country->id}}</th>
+                <td>{{ $country->name  }}</td>
+                <td>
+                    <a href="{{ route('country.edit',$country)  }}" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Editar</a>
+                </td>
+                <td>
+                    <form action="{{route('country.destroy',$country)}}" method="POST">
+                        @csrf @method('DELETE')
+                        <button type="submit" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Borrar</button>
+                    </form>
+                </td>
+            </tr>
         @endforeach
-    </ul>
+        </tbody>
+    </table>
 </x-layouts.layout-dashboard>
--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -1,9 +1,24 @@
 <x-layouts.layout-dashboard>
     <h1 class="text-4xl">Preguntas registradas</h1>
     <a href="{{route('questions.create')}}" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registrar nueva pregunta</a>
-    <ul>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Pregunta</th>
+            <th scope="col">Encuesta</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Borar</th>
+        </tr>
+        </thead>
+        <tbody>
         @foreach($questions as $question)
-            <li>{{ $question->question }} / <span>{{$question->survey->name}}</span></li>
+            <tr>
+                <th scope="row">{{ $question->id }}</th>
+                <td>{{ $question->question }}</td>
+                <td>{{ $question->survey->name }}</td>
+            </tr>
         @endforeach
-    </ul>
+        </tbody>
+    </table>
 </x-layouts.layout-dashboard>
